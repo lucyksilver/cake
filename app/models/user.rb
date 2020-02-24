79daf1618943f7c384e07eaf71ad260663a563d7
Lucy Silver <lucy.silver2@gmail.com>
2020-02-24 17:05:15 +0000

    routes done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :orders
+  has_many :items # user items for sale
+  has_many :items, through: :orders # ordered cakes by user
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :items do
+    resources :orders, only: [:new, :create]
+  end
+
+  resources :users, only: :show
+  resources :orders, only: :destroy
+
 end
