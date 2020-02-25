ced3da153bd9af60a97999acd6e774a93fff3806
Lucy Silver <lucy.silver2@gmail.com>
2020-02-25 13:58:39 +0000

    added an e on sponge

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   has_many :orders
   belongs_to :user
 
-  FLAVOURS = ["Lemon Drizzle", "Red Velvet", "Chocolate", "Carrot", "Sponge", "Chiffon", "Pineapple Upsidedown", "Victoria Spong", "Coffee and Walnut", "Vanilla", "Cheesecake", "Fruit", "Cinnamon", "Marble", "other"]
+  FLAVOURS = ["Lemon Drizzle", "Red Velvet", "Chocolate", "Carrot", "Sponge", "Chiffon", "Pineapple Upsidedown", "Victoria Sponge", "Coffee and Walnut", "Vanilla", "Cheesecake", "Fruit", "Cinnamon", "Marble", "other"]
   PORTIONS = ["1 - 4", "5 - 8", "9 - 14", "15 - 24", "25 - 99", "100"]
   OCCASIONS = ["Wedding Cake", "Birthday Cake", "Cupcakes", "Christmas Cake", "Anniversary Cake", "Valentines Cake", "Children’s Cake", "Other"]
 
