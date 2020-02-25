625602f5d7e49767bb64faee9dd8ba876b6b7a8b
Rahul Robertson <rahulrobertson@hotmail.com>
2020-02-25 12:31:08 +0000

    controller order destroy method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,14 @@
 class OrdersController < ApplicationController
   def new
+
   end
 
   def create
   end
 
-  def delete
+  def destroy
+    @order = Order.find(params[:id])
+    @order.delete
+    redirect_to items_path
   end
 end
