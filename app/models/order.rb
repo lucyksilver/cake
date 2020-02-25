7fa041b8c84118ef483793156e3c720d1bd8094d
Lucy Silver <lucy.silver2@gmail.com>
2020-02-25 10:54:27 +0000

    validations orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
+
+  # validates :description, :item_id, :user_id, presence: true
+  # validates :description, length: { minimum: 10 }
 end
