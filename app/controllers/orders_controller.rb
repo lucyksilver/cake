550dd204523e28c17446fab80ba3d01ca34d7ed5
Vanda Adams <vandamadams@gmail.com>
2020-02-25 12:27:14 +0000

    added strong params and new method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,17 @@
 class OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
 
   def create
   end
 
   def delete
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:description)
   end
 end
