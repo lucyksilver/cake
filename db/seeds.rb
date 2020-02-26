101ecd39c929e6e77b48735921350e9c0a5259a3
Vanda Adams <vandamadams@gmail.com>
2020-02-26 13:08:49 +0000

    changed flavours to names

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-NAMES = ["Vanilla Victoria Sponge", "Chocolate Fudge", "Lemon Drizzle", "Rainbow Cake", "Carrot Cake", "Red Velvet"]
+NAMES = ["Vanilla Victoria Sponge", "Chocolate Fudge Cake", "Lemon Drizzle Cake", "Rainbow Cake", "Carrot Cake", "Red Velvet Cake"]
 FLAVOURS = ["Vanilla", "Chocolate", "Lemon", "Strawberry", "Fruit", "Coffee", "Walnut", " Other"]
 PORTIONS = ["1-5", "5-10", "10-15", "15+", "25+", "50+", "75+", "100+"]
 OCCASIONS = ["Wedding", "Birthday", "Christmas", "Anniversary", "Valentines", "Children’s", "Other"]
 
 puts "Cleaning db"
+Order.destroy_all
 Item.destroy_all
 User.destroy_all
-Order.destroy_all
 
 puts "Creating users"
 user = User.create!(email: "tester@test.com", password: "123123", name: "Tester", about_me: "Some info about user")
