9f96746848fb8bc53afc7b1469bac1287c626b50
Lucy Silver <lucy.silver2@gmail.com>
2020-02-26 13:53:33 +0000

    edits

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,9 @@ class Item < ApplicationRecord
   has_many :orders
   belongs_to :user
 
-
+  FLAVOURS = ["Lemon Drizzle Cake", "Red Velvet Cake", "Chocolate Cake", "Carrot Cake", "Sponge Cake", "Chiffon Cake", "Pineapple Upsidedown Cake", "Victoria Sponge Cake", "Coffee and Walnut Cake", "Vanilla Cake", "Cheesecake Cake", "Fruit Cake", "Cinnamon Cake", "Marble Cake", "other"]
   PORTIONS = ["1 - 4", "5 - 8", "9 - 14", "15 - 24", "25 - 99", "100+"]
-  OCCASIONS = ["Wedding Cake", "Birthday Cake", "Cupcakes", "Christmas Cake", "Anniversary Cake", "Valentines Cake", "Children’s Cake", "Other"]
+  OCCASIONS = ["Wedding", "Birthday", "Cupcakes", "Christmas", "Anniversary", "Valentine's Day", "Children's Party", "Other"]
 
   $occasions = OCCASIONS
   $portions = PORTIONS
@@ -14,6 +14,7 @@ class Item < ApplicationRecord
   validates :flavour, presence: true, length: { minimum: 4, maximum: 40 }
   validates :portions, presence: true, inclusion: { in: PORTIONS }
   validates :occasion, presence: true, inclusion: { in: OCCASIONS }
+  validates :flavour, presence: true, inclusion: { in: FLAVOURS }
   validates :description, presence: true
   validates :name, presence: true
 end
