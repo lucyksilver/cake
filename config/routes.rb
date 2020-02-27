a3137c91bdafcc8953b11dcbc51d5c53d7944759
Lucy Silver <lucy.silver2@gmail.com>
2020-02-27 13:34:54 +0000

    edit and delete and photos added

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :items do
+  resources :items, except: :destroy do
     resources :orders, only: [:new, :create, :show]
   end
 
-  resources :users, only: [:show]
-  resources :orders, only: [:destroy]
+  resources :users, only: [:show, :edit, :update]
+  resources :orders, only: :destroy, as: 'order_delete'
+  resources :items, only: :destroy, as: 'item_delete'
 
   get "/profile", to: 'pages#profile'
 end
